0b77709bb20ef7865667e55f6cb8abea8c2e8e92
myrrhlin <1197072+myrrhlin@users.noreply.github.com>
2023-06-06 15:42:50 -0400

    use external file for ssh agent

--- dotf/agentup.sh
+++ dotf/agentup.sh
@@ -1,31 +1,32 @@
-# inspired by
+# this must be sourced -- cannot be run in subshell
+
 # https://stackoverflow.com/questions/18880024/start-ssh-agent-on-login
 
 SSH_ENV="$HOME/.ssh/env"
 
-function _init_agent {
+function _start_agent {
     echo "Initialising new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    echo succeeded
+    echo succeeded.  now:
     chmod 600 "${SSH_ENV}"
-    . "${SSH_ENV}" > /dev/null
+    source "${SSH_ENV}" > /dev/null
     # this requires entering a password...
-    echo /usr/bin/ssh-add "$HOME/.ssh/zipid"
+    echo /usr/bin/ssh-add "~/.ssh/zipid"
 }
 
-function ensure_agent {
+function _ensure_agent {
   if [ -f "$SSH_ENV" ]; then
      # env file exists, try to lock it
      #unset foo
      #exec {foo}<> "$SSH_ENV"
      #flock -n $foo
-     . "$SSH_ENV" > /dev/null
+     source "$SSH_ENV" > /dev/null
      if [ -n "$SSH_AGENT_PID" ]; then
         #ps ${SSH_AGENT_PID} doesn't work under cywgin
         ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-          # file was stale (no such agent running)
-          rm "$SSH_ENV"
-          _init_agent;
+          echo existing SSH_ENV file was stale -- starting new
+          # rm "$SSH_ENV"
+          _start_agent
         }
      else
        echo "SSH_ENV file was empty. source it later:"
@@ -33,31 +34,16 @@ function ensure_agent {
      fi
      # flock -u "$SSH_ENV"
   else
-    _init_agent;
+    _start_agent
   fi
 }
 
-function start_agent {
-	if [ -f "${SSH_ENV}" ]; then
-	   . "${SSH_ENV}" > /dev/null
-	   if [ -n "$SSH_AGENT_PID" ]; then
-	      #ps ${SSH_AGENT_PID} doesn't work under cywgin
-	      ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-		echo existing SSH_ENV file was stale -- starting new
-		_init_agent;
-	      }
-	   else
-	      echo "SSH_ENV file was empty. source it later:"
-	      echo "  source $SSH_ENV"
-	   fi
-	else
-	    _init_agent;
-	fi
+function randsleep {
+  local delay=3
+  perl -MTime::HiRes=sleep -e"sleep rand($delay)"
 }
 
-# Source SSH settings, if applicable
-
 #randomize execution time, so only one process writes the ENV file
-perl -MTime::HiRes=sleep -e'sleep rand(3)'
-start_agent;
+randsleep
+_ensure_agent
 
