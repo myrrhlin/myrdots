4314c2b026d2d3c77408344b9a0d976b3435c04f
myrrhlin <1197072+myrrhlin@users.noreply.github.com>
2023-06-06 15:42:49 -0400

    only set HISTFILE if it has default value

--- dotf/ehistory.bash
+++ dotf/ehistory.bash
@@ -4,11 +4,12 @@
 # http://stackoverflow.com/questions/9457233/unlimited-bash-history
 export HISTFILESIZE=
 export HISTSIZE=
-export HISTTIMEFORMAT="[%F %T] "
+HISTTIMEFORMAT="[%F %T] "
 # Change the file location because certain bash sessions truncate .bash_history file upon close.
 # http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
-if [ -z "$HISTFILE" ] ; then
-  export HISTFILE=~/.bash_eternal_history
+# if [ -z "$HISTFILE" ] ; then
+if [ "$HISTFILE" == ~/.bash_history ] ; then
+  HISTFILE=~/.bash_eternal_history
 fi
 # Force prompt to write history after every command.
 # http://superuser.com/questions/20900/bash-history-loss
